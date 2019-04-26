4f30fad45270330a144ed4e02d89e9258c18ce4c
delvedor <tommydelved@gmail.com>
2019-04-26 17:21:38 +0200

    Updated examples

--- examples/conditionals.ts
+++ examples/conditionals.ts
@@ -4,28 +4,16 @@ import { Q } from '../lib'
 async function run () {
   const client = new Client({ node: 'http://localhost:9200' })
 
-  // the result must be fixes done by delvedor
-  let query = Q.bool(
+  // you can handle conditionals parts of the query inline
+  // by using the 'and' operator
+  const query = Q.bool(
     Q.must(Q.match('description', 'fix')),
-    Q.filter(Q.term('author.name', 'delvedor'))
+    Q.filter(
+      // the term query will be present
+      // only if 'condition()' returns 'true'
+      condition() && Q.term('author.name', 'delvedor')
+    )
   )
-
-  // Based on a condition, we want to enrich our query
-  if (Math.random() >= 0.5) {
-    // the results must be fixes done by delvedor
-    // on test or do files
-    const should = Q.should(
-      Q.term('files', 'test'),
-      Q.term('files', 'docs')
-    )
-    query = Q.and(query, should)
-  } else {
-    // the results must be fixes or features done by delvedor
-    const must = Q.must(
-      Q.match('description', 'feature')
-    )
-    query = Q.or(query, must)
-  }
 
   const { body } = await client.search({
     index: 'git',
@@ -36,3 +24,7 @@ async function run () {
 }
 
 run().catch(console.log)
+
+function condition (): boolean {
+  return Math.random() >= 0.6
+}
