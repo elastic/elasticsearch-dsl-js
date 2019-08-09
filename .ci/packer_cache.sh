7d9f0b0905280ea91506068b48bdf3fb39d00d04
delvedor <tommydelved@gmail.com>
2019-08-09 14:31:55 +0200

    Added packer cache

---
+++ .ci/packer_cache.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source /usr/local/bin/bash_standard_lib.sh
+
+DOCKER_IMAGES="node:12-alpine
+node:10-alpine
+"
+
+for di in ${DOCKER_IMAGES}
+do
+(retry 2 docker pull "${di}") || echo "Error pulling ${di} Docker image, we continue"
+done
+
