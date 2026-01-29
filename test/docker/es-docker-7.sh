f93036956c3df8685df5fc4b84c976a5568efae9
Josh Mock <josh@joshmock.com>
2026-01-29 11:59:04 -0500

    Fresh start!

--- test/docker/es-docker-7.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-exec docker run \
-  --rm \
-  -e "discovery.type=single-node" \
-  -e "cluster.name=docker-cluster-7" \
-  -p 9207:9200 \
-  docker.elastic.co/elasticsearch/elasticsearch:7.3.0
